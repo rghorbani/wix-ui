d9ba82ba0c05cb0d5d323dd5b0bab4e21cc63315
Andrii Mokhovyk <andriimo@wix.com>
2018-09-28 12:42:05 +0300

    $VOD-3825|Change metadata name

--- packages/wix-ui-core/src/components/SocialPlayer/SocialPlayer.meta.ts
+++ packages/wix-ui-core/src/components/SocialPlayer/SocialPlayer.meta.ts
@@ -1,8 +1,8 @@
 import {SocialPlayer} from './SocialPlayer';
 import Registry from '@ui-autotools/registry';
 
-const socialVideoMetadata = Registry.getComponentMetadata(SocialPlayer);
-socialVideoMetadata
+const socialPlayerMetadata = Registry.getComponentMetadata(SocialPlayer);
+socialPlayerMetadata
   .addSim({
     title: 'Simulation with default props',
     props: {}
